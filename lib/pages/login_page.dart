007c956065b280ed02a410c69f550ed658cd5a07
Subhanshu Bansal <subhashu20135@iiitd.ac.in>
2023-06-03 13:37:39 +0530

    Added Component for the Textfield

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:wall_social_media_app_flutter/components/my_textfield.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
@@ -11,10 +12,31 @@ class LoginPage extends StatelessWidget {
         child: Center(
           child: Column(
             children: [
-              Icon(
+              const SizedBox(
+                height: 50,
+              ),
+              const Icon(
                 Icons.lock,
                 size: 100,
-              )
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              Text(
+                "Welcome Back! You\'ve been missed",
+                style: TextStyle(
+                  color: Colors.grey[700],
+                  fontSize: 16,
+                ),
+              ),
+              const SizedBox(
+                height: 25,
+              ),
+              MyTextField(),
+              const SizedBox(
+                height: 10,
+              ),
+              MyTextField()
             ],
           ),
         ),
